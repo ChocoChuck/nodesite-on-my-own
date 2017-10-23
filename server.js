3802939ff6fcd317099553dc14a55c3ffec413d1
ChocoChuck <coguagha1@gmail.com>
2017-10-22 22:05:13 -0400

    Server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -53,4 +54,6 @@ app.get('/about', function(req, res){
   });
 })
 
-app.listen(3000);
+app.listen(port, function(){
+  console.log('Server is on port' + port);
+});
